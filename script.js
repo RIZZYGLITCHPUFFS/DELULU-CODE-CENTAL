f37d333a7e0003b5e19b0ac6c1853b0aa3d4b87f
RGP <sergiolvasquez621@gmail.com>
2025-05-11 22:04:57 -0500

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function checkCode() {
     message.textContent = "I don't think nothing gives you something. That's all!";
     message.style.color = "orange";
   } else if (userInput === correctCode) {
-    message.textContent = "✅Congratulations! You've unlocked the Super-Secret Rizzy Glitch Revelation! ...Unfortunately, there's nothing here. You've been had! But hey, at least you ate some cereal, right? The cereal is all that matters..";
+    message.textContent = "✅Congratulations! You've unlocked the Super-Secret Rizzy Glitch Revelation! ...Unfortunately, there's nothing here. You've been had! But hey, at least you ate some cereal, right? The cereal is all that matters...";
     message.style.color = "green";
   } else {
     message.textContent = "❌ Hey, I think it's wrong. Before you try again, read this -> What are the strongest days of the week? Saturday and Sunday, the rest are weekdays.";
