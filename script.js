f59438d67457c09b18c9a76b0145e630c96307f1
RGP <sergiolvasquez621@gmail.com>
2025-05-12 11:29:50 -0500

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function checkCode() {
   const correctCode = "DELULU-4-LYFE";
+  const correctCode2 = "delulu-4-lyfe";
   const userInput = document.getElementById("codeInput").value;
   const message = document.getElementById("message");
 
   if (userInput === "") {
     message.textContent = "I don't think nothing gives you something. That's all!";
     message.style.color = "orange";
-  } else if (userInput === correctCode) {
+  } else if (userInput === correctCode || userInput === correctCode2) {
     message.textContent = "✅Congratulations! You've unlocked the Super-Secret Rizzy Glitch Revelation! ...Unfortunately, there's nothing here. You've been had! But hey, at least you ate some cereal, right? The cereal is all that matters...";
     message.style.color = "green";
   } else {
